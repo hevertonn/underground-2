b692a204486146eb1c60aceab4d766e791810cba
Heverton <hb.hevertonborges@gmail.com>
2026-07-10 20:55:41 -0300

    Update point class to use semaphore arraylists

--- model/Point.java
+++ model/Point.java
@@ -9,12 +9,13 @@
 
 package model;
 
+import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 public class Point {
   private double[] coordinates = new double[2];
-  private Semaphore[] in = new Semaphore[0];
-  private Semaphore[] out = new Semaphore[0];
+  private ArrayList<Semaphore> in = new ArrayList<>();
+  private ArrayList<Semaphore> out = new ArrayList<>();
 
   public Point(double[] coordinates) {
     this.coordinates = coordinates;
@@ -24,19 +25,11 @@ public class Point {
     return coordinates;
   }
 
-  public Semaphore[] getIn() {
+  public ArrayList<Semaphore> getIn() {
     return in;
   }
 
-  public Semaphore[] getOut() {
+  public ArrayList<Semaphore> getOut() {
     return out;
-  }
-
-  public void setIn(Semaphore... in) {
-    this.in = in;
-  }
-
-  public void setOut(Semaphore... out) {
-    this.out = out;
   }
 }
